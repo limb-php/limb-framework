b8e31abdea406b398c1a72a68dc72b223b6a9df8
Roman B <xadminx@mail.ru>
2021-10-23 21:14:40 +0300

    minor changes

--- web_app/src/exception/lmbDelegateHaltException.class.php
+++ web_app/src/exception/lmbDelegateHaltException.class.php
@@ -6,7 +6,9 @@
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
  */
-lmb_require('limb/core/src/exception/lmbException.class.php');
+namespace limb\web_app\src\exception;
+
+use limb\core\src\exception\lmbException;
 
 /**
  * class lmbDelegateHaltException.
