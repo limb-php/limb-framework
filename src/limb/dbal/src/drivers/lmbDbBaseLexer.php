926b8ca2a0d9d6ed0e1b69d122d728c7881faced
AnonDev <AnonDev@gmail.com>
2022-09-21 15:50:08 +0300

    -- dbal: lexer added

---
+++ src/limb/dbal/src/drivers/lmbDbBaseLexer.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+namespace limb\dbal\src\drivers;
+
+use limb\core\src\exception\lmbException;
+use limb\dbal\src\query\lmbQueryLexerInterface;
+
+/**
+ * class lmbDbBaseLexer
+ *
+ * @package dbal
+ * @version $Id$
+ */
+abstract class lmbDbBaseLexer implements lmbQueryLexerInterface
+{
+    function getSelectQueryTemplate()
+    {
+        return 'SELECT %fields% FROM %tables% %left_join% %where% %group% %having% %order%';
+    }
+
+    function getInsertQueryTemplate()
+    {
+        return 'INSERT INTO %table% (%fields%) VALUES (%values%)';
+    }
+
+    function getUpdateQueryTemplate()
+    {
+        return 'UPDATE %table% SET %fields% %where%';
+    }
+
+    function getbDeleteQueryTemplate()
+    {
+        return 'DELETE FROM %table% %where%';
+    }
+
+    function getInsertOnDuplicateQueryTemplate()
+    {
+        throw new lmbException('Not supported on this DB');
+    }
+}
---
+++ src/limb/dbal/src/query/lmbQueryLexerInterface.php
@@ -0,0 +1,22 @@
+<?php
+namespace limb\dbal\src\query;
+
+/**
+ * interface lmbQueryLexerInterface.
+ *
+ * @package dbal
+ * @version $Id: lmbQueryLexerInterface 7486 2009-01-26 19:13:20Z
+ */
+interface lmbQueryLexerInterface
+{
+    function getSelectQueryTemplate();
+
+    function getInsertQueryTemplate();
+
+    function getUpdateQueryTemplate();
+
+    function getbDeleteQueryTemplate();
+
+    function getInsertOnDuplicateQueryTemplate();
+}
+
