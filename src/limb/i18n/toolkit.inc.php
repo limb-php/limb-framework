5cef7111e143ac52c71ed80d1d13c17061f629de
AnonDev <AnonDev@gmail.com>
2022-10-22 15:48:36 +0300

    Create toolkit.inc.php
    toolkit.inc.php for i18n

---
+++ src/limb/i18n/toolkit.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * @package i18n
+ * @version $Id: toolkit.inc.php 2022-10-22
+ */
+
+use limb\toolkit\src\lmbToolkit;
+use limb\i18n\src\toolkit\lmbI18NTools;
+
+lmbToolkit::merge(new lmbI18NTools());
