13f884d7457f352cc5c809d1d542dafa63890176
Roman B <xadminx@mail.ru>
2022-10-29 23:08:04 +0300

    Update lmbWebApplication.php

--- src/limb/web_app/src/lmbWebApplication.php
+++ src/limb/web_app/src/lmbWebApplication.php
@@ -10,16 +10,24 @@ namespace limb\web_app\src;
 
 use limb\filter_chain\src\lmbFilterChain;
 use limb\core\src\lmbHandle;
+use limb\web_app\src\controller\NotFoundController;
+use limb\web_app\src\request\lmbRoutesRequestDispatcher;
+use limb\web_app\src\filter\lmbErrorHandlingFilter;
+use limb\web_app\src\filter\lmbSessionStartupFilter;
+use limb\web_app\src\filter\lmbRequestDispatchingFilter;
+use limb\web_app\src\filter\lmbResponseTransactionFilter;
+use limb\web_app\src\filter\lmbActionPerformingFilter;
+use limb\web_app\src\filter\lmbViewRenderingFilter;
 
 /**
  * class lmbWebApplication.
  *
  * @package web_app
- * @version $Id: lmbWebApplication.class.php 7681 2009-03-04 05:58:40Z pachanga $
+ * @version $Id: lmbWebApplication.php 7681 2009-03-04 05:58:40Z
  */
 class lmbWebApplication extends lmbFilterChain
 {
-  protected $default_controller_name = "limb\web_app\src\controller\NotFoundController";
+  protected $default_controller_name = NotFoundController::class;
   protected $pre_dispatch_filters = array();
   protected $pre_action_filters = array();
   protected $pre_view_filters = array();
@@ -38,7 +46,7 @@ class lmbWebApplication extends lmbFilterChain
   protected function _getRequestDispatcher()
   {
     if(!is_object($this->request_dispatcher))
-      return new lmbHandle('limb\web_app\src\request\lmbRoutesRequestDispatcher');
+      return new lmbHandle(lmbRoutesRequestDispatcher::class);
     return $this->request_dispatcher;
   }
 
@@ -65,23 +73,24 @@ class lmbWebApplication extends lmbFilterChain
 
   protected function _registerFilters()
   {
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbErrorHandlingFilter'));
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbSessionStartupFilter'));
+    $this->registerFilter(new lmbHandle(lmbErrorHandlingFilter::class));
+
+    $this->registerFilter(new lmbHandle(lmbSessionStartupFilter::class));
 
     $this->_addFilters($this->pre_dispatch_filters);
 
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbRequestDispatchingFilter',
+    $this->registerFilter(new lmbHandle(lmbRequestDispatchingFilter::class,
                                         array($this->_getRequestDispatcher(),
                                               $this->default_controller_name)));
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbResponseTransactionFilter'));
+    $this->registerFilter(new lmbHandle(lmbResponseTransactionFilter::class));
 
     $this->_addFilters($this->pre_action_filters);
 
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbActionPerformingFilter'));
+    $this->registerFilter(new lmbHandle(lmbActionPerformingFilter::class));
 
     $this->_addFilters($this->pre_view_filters);
 
-    $this->registerFilter(new lmbHandle('limb\web_app\src\filter\lmbViewRenderingFilter'));
+    $this->registerFilter(new lmbHandle(lmbViewRenderingFilter::class));
   }
 
   protected function _addFilters($filters)
@@ -89,7 +98,4 @@ class lmbWebApplication extends lmbFilterChain
     foreach($filters as $filter)
       $this->registerFilter($filter);
   }
-
 }
-
-
