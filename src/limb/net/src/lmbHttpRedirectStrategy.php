3c9709f74d1d05875d46d8d76d2cc0d4733687ae
Roman B <xadminx@mail.ru>
2022-11-04 23:47:58 +0300

    Update lmbHttpRedirectStrategy.php

--- src/limb/net/src/lmbHttpRedirectStrategy.php
+++ src/limb/net/src/lmbHttpRedirectStrategy.php
@@ -12,14 +12,12 @@ namespace limb\net\src;
  * class lmbHttpRedirectStrategy.
  *
  * @package net
- * @version $Id: lmbHttpRedirectStrategy.class.php 7486 2009-01-26 19:13:20Z pachanga $
+ * @version $Id: lmbHttpRedirectStrategy.php 7486 2009-01-26 19:13:20Z
  */
 class lmbHttpRedirectStrategy
 {
   function redirect($response, $path)
   {
-    $response->addHeader("Location: {$path}");
+    $response->addHeader("Location", $path);
   }
 }
-
-
