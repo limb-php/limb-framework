00b172aafdb8d3c1178eae3b28fd63505ee3563a
Roman B <xadminx@mail.ru>
2022-12-06 01:04:43 +0300

    Update lmbValidatorBuilder.php

--- src/limb/validation/src/lmbValidatorBuilder.php
+++ src/limb/validation/src/lmbValidatorBuilder.php
@@ -142,7 +142,9 @@ class lmbValidatorBuilder
 
     $path_to_rule = self::getPathByRuleName($rule_name);
 
-    return new lmbHandle($path_to_rule, $params);
+    $class_name = self::getClassNameInFile($path_to_rule);
+
+    return new lmbHandle($class_name, $params);
   }
 
   static function getPathByRuleName($rule_name) 
@@ -161,6 +163,43 @@ class lmbValidatorBuilder
     
     throw new lmbException("Rule $rule_name ($rule_file_name) was not found in " . lmbEnv::get('LIMB_RULES_INCLUDE_PATH'));
   }
+
+    static function getClassNameInFile($file)
+    {
+        $fp = fopen($file, 'r');
+        $class = $namespace = $buffer = '';
+        $i = 0;
+        while (!$class) {
+            if (feof($fp)) break;
+
+            $buffer .= fread($fp, 512);
+            $tokens = token_get_all($buffer);
+
+            if (strpos($buffer, '{') === false) continue;
+
+            for (;$i<count($tokens);$i++) {
+                if ($tokens[$i][0] === T_NAMESPACE) {
+                    for ($j=$i+1;$j<count($tokens); $j++) {
+                        if ($tokens[$j][0] === T_STRING) {
+                            $namespace .= $tokens[$j][1] . '\\';
+                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                            break;
+                        }
+                    }
+                }
+
+                if ($tokens[$i][0] === T_CLASS) {
+                    for ($j=$i+1;$j<count($tokens);$j++) {
+                        if ($tokens[$j] === '{') {
+                            $class = $tokens[$i+2][1];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $namespace . $class;
+    }
 
   static function getLmbRule($underscored_name) 
   {
