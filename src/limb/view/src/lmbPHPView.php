26247703ec648b348f5e2d28b77b586c8f0f3490
AnonDev <AnonDev@gmail.com>
2023-05-31 17:46:46 +0300

    Update lmbPHPView.php
    -- add locateTemplateByAlias() method to lmbPHPView

--- src/limb/view/src/lmbPHPView.php
+++ src/limb/view/src/lmbPHPView.php
@@ -6,7 +6,11 @@
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
  */
+
 namespace limb\view\src;
+
+use limb\fs\src\lmbFs;
+use limb\toolkit\src\lmbToolkit;
 
 /**
  * class lmbPHPView.
@@ -15,14 +19,26 @@ namespace limb\view\src;
  * @version $Id$
  */
 class lmbPHPView extends lmbView
-{  
-  function render()
-  {
-    extract($this->getVariables());
-    ob_start();
-    include($this->getTemplate());
-    $res = ob_get_contents();
-    ob_end_clean();
-    return $res;
-  }
+{
+    static function locateTemplateByAlias($alias)
+    {
+        if(!lmbFs::isPathAbsolute($alias))
+            return lmbToolkit::instance()->tryFindFileByAlias($alias, ['template'], 'php');
+        elseif(file_exists($alias))
+            return $alias;
+
+        return false;
+    }
+
+    function render()
+    {
+        extract($this->getVariables());
+        ob_start();
+
+        include( self::locateTemplateByAlias($this->getTemplate()) );
+
+        $res = ob_get_contents();
+        ob_end_clean();
+        return $res;
+    }
 }
