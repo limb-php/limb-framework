80ede72de4d0134536be064b97af7969adf676a0
Roman B <xadminx@mail.ru>
2023-06-03 09:43:48 +0300

    Update lmbCmsRequestDispatchingFilter.php
    -- minor fix

--- src/limb/cms/src/filter/lmbCmsRequestDispatchingFilter.php
+++ src/limb/cms/src/filter/lmbCmsRequestDispatchingFilter.php
@@ -6,8 +6,10 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
  */
+
 namespace limb\cms\src\filter;
 
+use limb\web_app\src\Controllers\NotFoundController;
 use limb\web_app\src\filter\lmbRequestDispatchingFilter;
 use limb\web_app\src\request\lmbCompositeRequestDispatcher;
 use limb\cms\src\request\lmbCmsDocumentRequestDispatcher;
@@ -21,13 +23,12 @@ use limb\web_app\src\request\lmbRoutesRequestDispatcher;
  */
 class lmbCmsRequestDispatchingFilter extends lmbRequestDispatchingFilter
 {
-  function __construct($default_controller_name = 'NotFoundController')
-  {
-    $dispatcher = new lmbCompositeRequestDispatcher();
-    
-    $dispatcher->addDispatcher(new lmbCmsDocumentRequestDispatcher());
-    $dispatcher->addDispatcher(new lmbRoutesRequestDispatcher());
-    
-    parent::__construct($dispatcher, $default_controller_name);
-  }
+    function __construct($default_controller_name = NotFoundController::class)
+    {
+        $dispatcher = new lmbCompositeRequestDispatcher();
+        $dispatcher->addDispatcher(new lmbCmsDocumentRequestDispatcher());
+        $dispatcher->addDispatcher(new lmbRoutesRequestDispatcher());
+
+        parent::__construct($dispatcher, $default_controller_name);
+    }
 }
