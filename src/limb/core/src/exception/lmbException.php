717cb4b3b74526e3d0c95aef1ff6ed9b96f434cb
AnonDev <AnonDev@gmail.com>
2023-07-03 15:12:23 +0300

    Update lmbException.php

--- src/limb/core/src/exception/lmbException.php
+++ src/limb/core/src/exception/lmbException.php
@@ -6,6 +6,7 @@
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
+
 namespace limb\core\src\exception;
 
 use limb\core\src\lmbBacktrace;
@@ -18,93 +19,78 @@ use limb\core\src\lmbBacktrace;
  */
 class lmbException extends \Exception
 {
-  protected $original_message;
-  protected $params = [];
-  //protected $file;
-  //protected $line;
-  protected $backtrace;
+    protected $original_message;
+    protected $params = [];
+    protected $backtrace;
 
-  function __construct($message, $params = array(), $code = 0, $hide_calls_count = 0)
-  {
-    $this->original_message = $message;
-    if(is_array($params) && sizeof($params))
+    function __construct($message, $params = array(), $code = 0, $hide_calls_count = 0)
     {
-      $message .= "\n[params: " . var_export($params, true) . "]\n";
+        $this->original_message = $message;
+        if (is_array($params) && sizeof($params)) {
+            $message .= "\n[params: " . var_export($params, true) . "]\n";
+        }
+
+        $this->params = $params;
+        $this->backtrace = array_slice(debug_backtrace(), $hide_calls_count);
+
+        foreach ($this->backtrace as $item) {
+            if (isset($item['file'])) {
+                $this->file = $item['file'];
+                $this->line = $item['line'];
+                break;
+            }
+        }
+
+        parent::__construct($message, $code);
     }
 
-    $this->params = $params;
-    $this->backtrace = array_slice(debug_backtrace(), $hide_calls_count);
-
-    foreach($this->backtrace as $item)
+    function getOriginalMessage()
     {
-      if (isset($item['file']))
-      {
-        $this->file = $item['file'];
-        $this->line = $item['line'];
-        break;
-      }
+        return $this->original_message;
     }
 
-    parent::__construct($message, $code);
-  }
+    function getParams()
+    {
+        return $this->params;
+    }
 
-  function getOriginalMessage()
-  {
-    return $this->original_message;
-  }
+    function getParam($name)
+    {
+        if (isset($this->params[$name]))
+            return $this->params[$name];
+    }
 
-  function getRealFile()
-  {
-    return $this->file;
-  }
+    function getBacktrace()
+    {
+        return $this->backtrace;
+    }
 
-  function getRealLine()
-  {
-    return $this->line;
-  }
+    function getNiceTraceAsString()
+    {
+        return $this->getBacktraceObject()->toString();
+    }
 
-  function getParams()
-  {
-    return $this->params;
-  }
+    /**
+     * @return lmbBacktrace
+     */
+    function getBacktraceObject()
+    {
+        return new lmbBacktrace($this->backtrace);
+    }
 
-  function getParam($name)
-  {
-    if(isset($this->params[$name]))
-      return $this->params[$name];
-  }
+    function toNiceString($without_backtrace = false): string
+    {
+        $string = '';
+        $string .= get_class($this) . ': ' . $this->getOriginalMessage() . PHP_EOL;
+        if ($this->params)
+            $string .= 'Additional params: ' . strstr(print_r($this->params, true), PHP_EOL);
+        if (!$without_backtrace)
+            $string .= 'Backtrace: ' . PHP_EOL . $this->getBacktraceObject()->toString();
+        return $string;
+    }
 
-  function getBacktrace()
-  {
-    return $this->backtrace;
-  }
-
-  function getNiceTraceAsString()
-  {
-    return $this->getBacktraceObject()->toString();
-  }
-
-  /**
-   * @return lmbBacktrace
-   */
-  function getBacktraceObject()
-  {
-    return new lmbBacktrace($this->backtrace);
-  }
-
-  function toNiceString($without_backtrace = false)
-  {
-    $string = '';
-    $string .= get_class($this).': '.$this->getOriginalMessage().PHP_EOL;
-    if($this->params)
-        $string .= 'Additional params: '.strstr(print_r($this->params, true), PHP_EOL);
-    if(!$without_backtrace)
-      $string .= 'Backtrace: '.PHP_EOL.$this->getBacktraceObject()->toString();
-    return $string;
-  }
-
-  function __toString()
-  {
-    return $this->toNiceString();
-  }
+    function __toString()
+    {
+        return $this->toNiceString();
+    }
 }
