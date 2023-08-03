3dcb63af0709484159c246063ae2a60e42864ae5
Roman B <xadminx@mail.ru>
2023-08-04 00:19:36 +0300

    -- web_app: tests. paths

--- tests/web_app/cases/.setup.php
+++ tests/web_app/cases/.setup.php
@@ -1,8 +1,8 @@
 <?php
 
 use limb\core\src\lmbEnv;
-
-lmbEnv::setor('LIMB_CONF_INCLUDE_PATH', 'settings;limb/*/settings;limb/config/tests/cases/settings');
+use limb\toolkit\src\lmbToolkit;
+use limb\view\src\lmbPHPView;
 
 require_once(dirname(__FILE__) . '/../../../src/limb/core/common.inc.php');
 require_once(dirname(__FILE__) . '/../../../src/limb/macro/common.inc.php');
@@ -12,6 +12,10 @@ require_once(dirname(__FILE__) . '/../../../src/limb/web_app/toolkit.inc.php');
 require_once(dirname(__FILE__) . '/../../core/common.inc.php');
 require_once(dirname(__FILE__) . '/../../dbal/common.inc.php');
 require_once(dirname(__FILE__) . '/../../active_record/cases/init.inc.php');
+
+lmbEnv::set('LIMB_CONF_INCLUDE_PATH', __DIR__ . '/settings;' . __DIR__ . '/../../*/cases/settings');
+
+lmbToolkit::instance()->setSupportedViewTypes(array('.php' => lmbPHPView::class));
 
 lmb_tests_init_var_dir(dirname(__FILE__) . '/../../../var/web_app');
 
--- tests/web_app/cases/plain/macro/.setup.php
+++ tests/web_app/cases/plain/macro/.setup.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once '../../.setup.php';
+require_once __DIR__ . '/../../.setup.php';
