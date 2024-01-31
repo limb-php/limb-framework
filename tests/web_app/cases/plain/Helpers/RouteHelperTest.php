ddf7071a482c1c37d01e3b6b40cd58a47ff3e8c9
AnonDev <AnonDev@gmail.com>
2024-01-31 17:34:54 +0300

    -- web_app: tests

---
+++ tests/web_app/cases/plain/Helpers/RouteHelperTest.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+namespace Tests\web_app\cases\plain\Helpers;
+
+use limb\web_app\src\Helpers\lmbRouteHelper;
+use PHPUnit\Framework\TestCase;
+use Tests\web_app\cases\plain\src\controller\api\NewIndexController;
+use Tests\web_app\cases\plain\src\Controllers\Api\ApiTestingController;
+use Tests\web_app\cases\plain\src\Controllers\SecondTestingController;
+
+require dirname(__FILE__) . '/../../.setup.php';
+
+class RouteHelperTest extends TestCase
+{
+
+    function testControllerNameByClass()
+    {
+        $cname = lmbRouteHelper::getControllerNameByClass(new ApiTestingController());
+        $this->assertEquals('api.api_testing', $cname);
+
+        $cname = lmbRouteHelper::getControllerNameByClass(new SecondTestingController());
+        $this->assertEquals('second_testing', $cname);
+
+        $cname = lmbRouteHelper::getControllerNameByClass(new NewIndexController());
+        $this->assertEquals('api.new_index', $cname);
+    }
+
+}
---
+++ tests/web_app/cases/plain/src/controller/Api/NewIndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\web_app\cases\plain\src\controller\api;
+
+use limb\web_app\src\Controllers\LmbController;
+
+class NewIndexController extends LmbController
+{
+
+}
