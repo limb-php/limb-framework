413163d7c3fa317f22559a75880754859444d126
Roman B <xadminx@mail.ru>
2024-04-01 23:07:37 +0300

    Create common.conf.php

---
+++ src/limb/log/settings/common.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+use limb\core\src\lmbEnv;
+
+$conf = [
+
+    'logs' => [
+        'error' => [
+            'file://' . lmbEnv::get('LIMB_VAR_DIR') . 'log/error.log'
+        ],
+
+        'db' => [
+            'file://' . lmbEnv::get('LIMB_VAR_DIR') . 'log/db.log'
+        ],
+    ]
+
+];
