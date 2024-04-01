b425a2b4c998de08cea7702fe805c97abb54d50d
Roman B <xadminx@mail.ru>
2024-04-01 21:08:01 +0300

    dbal: init test

--- tests/dbal/cases/init.inc.php
+++ tests/dbal/cases/init.inc.php
@@ -7,10 +7,11 @@ require_once(dirname(__FILE__) . '/init.inc.php');
 require_once(dirname(__FILE__) . '/../../../src/limb/dbal/toolkit.inc.php');
 
 use limb\core\src\lmbEnv;
-use limb\toolkit\src\lmbToolkit;
-use limb\dbal\src\toolkit\lmbDbTools;
+use Psr\Log\LogLevel;
 
 $LIMB_CONF_INCLUDE_PATH = lmbEnv::get('LIMB_CONF_INCLUDE_PATH');
 lmbEnv::set('LIMB_CONF_INCLUDE_PATH', dirname(__FILE__) . '/settings;' . $LIMB_CONF_INCLUDE_PATH);
+
+lmbEnv::set('LIMB_LOG_LEVEL', LogLevel::DEBUG);
 
 lmb_tests_init_var_dir(dirname(__FILE__) . '/../../../var/dbal/');
