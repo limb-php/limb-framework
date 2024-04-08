ba88236dd5b9b5df1b53ed378dd3f2194fa9f9d9
AnonDev <AnonDev@gmail.com>
2024-04-08 20:19:19 +0300

    Update .setup.php
    -- search: tests fix

--- tests/search/cases/.setup.php
+++ tests/search/cases/.setup.php
@@ -2,17 +2,17 @@
 
 use limb\core\src\lmbEnv;
 use limb\dbal\src\toolkit\lmbDbTools;
+use limb\log\src\toolkit\lmbLogTools;
 use limb\toolkit\src\lmbToolkit;
 
 require_once(dirname(__FILE__) . '/../../../src/limb/core/common.inc.php');
 require_once(dirname(__FILE__) . '/../../../src/limb/search/common.inc.php');
 require_once(dirname(__FILE__) . '/../../core/common.inc.php');
 require_once(dirname(__FILE__) . '/../../dbal/common.inc.php');
+require_once(dirname(__FILE__) . '/../../../src/limb/dbal/toolkit.inc.php');
 
 $LIMB_CONF_INCLUDE_PATH = lmbEnv::get('LIMB_CONF_INCLUDE_PATH');
 lmbEnv::set('LIMB_CONF_INCLUDE_PATH', dirname(__FILE__) . '/settings;' . $LIMB_CONF_INCLUDE_PATH);
-
-lmbToolkit::merge(new lmbDbTools());
 
 lmb_tests_init_var_dir(dirname(__FILE__) . '/../../../var/search');
 
