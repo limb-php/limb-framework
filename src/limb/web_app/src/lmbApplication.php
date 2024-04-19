ede62a3969b50ece1799bdd43c7667db74d1f023
Roman B <xadminx@mail.ru>
2024-04-19 23:01:08 +0300

    Update lmbApplication.php
    -- request fix

--- src/limb/web_app/src/lmbApplication.php
+++ src/limb/web_app/src/lmbApplication.php
@@ -46,31 +46,33 @@ class lmbApplication
     function process($request): ResponseInterface
     {
         try {
-            $this->_registerDispatcher();
-            $this->_registerMiddleware();
             $this->_registerBootstraps();
+            $this->_registerMiddleware();
+            $this->_registerDispatcher();
 
             $this->_bootstrap($request);
 
             $response = $this->middleware->process($request, function ($request) {
-                //$dispatched = lmbToolkit::instance()->getDispatchedController();
-                $dispatched = $this->_getDispatchedController($request);
-                lmbToolkit::instance()->setDispatchedController($dispatched);
+                $dispatched_params = $this->dispatcher->dispatch($request);
+                foreach ($dispatched_params as $name => $value)
+                    $request = $request->withAttribute($name, $value);
+                lmbToolkit::instance()->setRequest($request);
+
+                $dispatched = $this->_createController($dispatched_params);
 
                 return $this->_callControllerAction($dispatched, $request);
             });
 
             $this->_terminate();
-        }
-        catch (\Throwable $e) {
-            if( $e instanceof \Error ) {
+        } catch (\Throwable $e) {
+            if ($e instanceof \Error) {
                 $error = error_get_last();
-                if($error) {
+                if ($error) {
                     //$flags = [E_ERROR, E_CORE_ERROR, E_USER_ERROR, E_COMPILE_ERROR, E_RECOVERABLE_ERROR];
                     $flags = [E_ERROR];
 
                     foreach ($flags as $flag) {
-                        if( $error['type']&$flag )
+                        if ($error['type'] & $flag)
                             return $this->handler->handleFatalError($error, $request);
                     }
                 }
@@ -90,7 +92,6 @@ class lmbApplication
     protected function _bootstrap($request)
     {
         foreach ($this->bootstraps as $bootstrap) {
-
             if (is_callable([$bootstrap, 'bootstrap'])) {
                 $bootstrap->bootstrap($request);
             }
@@ -105,18 +106,6 @@ class lmbApplication
         }
     }
 
-    protected function _getDispatchedController($request)
-    {
-        $dispatched_params = $this->dispatcher->dispatch($request);
-
-//        foreach ($dispatched_params as $name => $value) {
-//            $request = $request->withAttribute($name, $value);
-//        }
-//        lmbToolkit::instance()->setRequest($request);
-
-        return $this->_createController($dispatched_params);
-    }
-
     protected function _createController($dispatched_params)
     {
         try {
@@ -125,6 +114,8 @@ class lmbApplication
         } catch (lmbControllerNotFoundException $e) {
             $controller = $this->_createDefaultController();
         }
+
+        lmbToolkit::instance()->setDispatchedController($controller);
 
         return $controller;
     }
@@ -137,7 +128,7 @@ class lmbApplication
         return $controller;
     }
 
-    protected function _callControllerAction($dispatched, $request)
+    protected function _callControllerAction($dispatched, $request): ResponseInterface
     {
         if (!is_object($dispatched)) {
             throw new lmbException('Request is not dispatched yet! lmbDispatchedRequest not found in lmbToolkit!');
