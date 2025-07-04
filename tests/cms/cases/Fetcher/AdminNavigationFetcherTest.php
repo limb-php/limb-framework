d999b0d105fbc9dd79c93cf8a8ed4f79cd682b12
Roman B <xadminx@mail.ru>
2025-07-05 02:13:02 +0300

    -- cms: tests

---
+++ tests/cms/cases/Fetcher/AdminNavigationFetcherTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tests\cms\cases\Fetcher;
+
+use limb\cms\src\Auth\lmbAuth;
+use limb\cms\src\fetcher\lmbCmsAdminNavigationFetcher;
+use limb\cms\src\model\lmbCmsUserRoles;
+use limb\toolkit\src\lmbToolkit;
+use PHPUnit\Framework\TestCase;
+
+require_once(dirname(__FILE__) . '/../.setup.php');
+
+class AdminNavigationFetcherTest extends TestCase
+{
+    function testNavigationFetcher()
+    {
+        $toolkit = lmbToolkit::instance();
+        $conf = $toolkit->getConf('navigation');
+
+        $admin_nav = (new lmbCmsAdminNavigationFetcher)->fetch();
+        $this->assertEquals(null, $admin_nav['title']);
+
+        lmbAuth::login('admin', 'secret');
+
+        $admin_nav2 = (new lmbCmsAdminNavigationFetcher)->fetch();
+        $this->assertEquals($conf[lmbCmsUserRoles::ADMIN][1], $admin_nav2[1]);
+    }
+}
